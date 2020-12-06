b3d4cb59e007e8c3c93b93167298d95206390ecf
sangwoo <dev.sangwoo@gmail.com>
2020-12-07 02:03:41 +0900

    Add personal information

--- src/components/Introduce/index.jsx
+++ src/components/Introduce/index.jsx
@@ -19,11 +19,19 @@ const Introduce = ({ className }) => {
     <Card className={classProps} title='이력서'>
       <span className={styles.title1}>웹 프론트엔드 개발자</span>
       <span className={styles.title2}>조상우</span>
-      {/* <span className={styles.title3}>이력서</span> */}
       <div className={styles.descriptions}>
         <p className={styles.description}>도쿄에서 웹 프론트엔드 엔지니어로 일하고 있으며, 특히 유저 입장에서 만족할 수 있는 UI 설계를 목표하고 있습니다. </p>
         <p className={styles.description}>비즈니스 로직과 가치를 이해하려고 노력하는 개발자입니다. 주도적으로 업무를 진행할 수 있는 환경을 선호하고 도전하고 싶습니다.</p>
       </div>
+      <div className={styles.descriptions}>
+        <p className={styles.title3}>- 생년월일 : 1991년 02년 26일 (만 29세)</p>
+        <p className={styles.title3}>- 한국주소 : 전라북도 전주시 완산구 삼천동 1가 흥건아파트 101/1702</p>
+        <p className={styles.title3}>- 전화번호 : (+82) 010 - 3668 - 0476</p>
+        <p className={styles.title3}>-  이메일  : sangwoodev@gmail.com</p>
+      </div>
+      
+
+      
       <div className={styles.iconWrap}>
         <a className={styles.link} href={config.github}>
           <img className={styles.img} src={GithubIcon} alt='github-icon' />
